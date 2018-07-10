aeb6fad00bbfa801e7ff047cb19d8aa0be43ff20
Thomas Ibbotson <thomas.ibbotson@gmail.com>
2018-07-10 13:35:30 +0200

    cd to the right directory

--- run-backup.sh
+++ run-backup.sh
@@ -2,4 +2,5 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=/home/rscds/website-backups/credentials/google_app_creds.json
 
+cd /home/rscds/website-backups
 php /home/rscds/website-backups/backup.php $*
